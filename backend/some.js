c6014494bded5880e9bbe2ac9e5e994bf7db66eb
cvamyadav <shivam842926.yadav@gmail.com>
2025-08-14 09:14:03 +0000

    soe1

--- backend/some.js
+++ backend/some.js
@@ -5,7 +5,7 @@ const app = express();
 
 const port = 3001;
 app.use(cors({
-  origin: ['http://localhost:3000','https://weather-pi-rosy.vercel.app' ]
+  origin: ['http://localhost:3000','https://weather-pi-rosy.vercel.app' ,'https://weather-xi-beryl-29.vercel.app/']
 }));
 app.use(express.json());
 
